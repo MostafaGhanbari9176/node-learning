3338d34640d0a63f6c42fb78ff1c569646f77c4f
Mostafa <godhelot1@outlook.com>
2020-11-01 00:16:30 +0330

    use pdfKit to create dynamic

--- controllers/orders.js
+++ controllers/orders.js
@@ -1,5 +1,6 @@
 const fs = require('fs')
 const path = require('path')
+const PdfDocument = require('pdfkit')
 
 const Order = require('../models/order')
 
@@ -33,7 +34,6 @@ exports.postCreateOrder = (req, res) => {
 exports.getOrderList = (req, res) => {
     Order.find({user: req.user})
         .then(orders => {
-            console.log(orders)
             res.render('./order.ejs', {
                 pageTitle: "Orders",
                 orders: orders
@@ -49,13 +49,37 @@ exports.releasePdf = (req, res, next) => {
             if (!order)
                 throw new Error('not order found!')
             if (order.user.toString() === req.user._id.toString()) {
-                const docPath = path.join('data', 'order_docs', 'order-' + orderId + '.pdf')
-                const file = fs.createReadStream(docPath)
-                res.setHeader('Content-Type', 'application/pdf')
-                res.setHeader('Content-Disposition', 'filename="order-'+orderId+'.pdf"')
-                file.pipe(res)
+                createPdfDoc(req, res, order)
             } else
                 throw new Error('access denied ❌')
         })
         .catch(err => next(err))
+}
+
+const createPdfDoc = (req, res, order) => {
+    const orderId = req.params.orderId
+
+    const orderName = 'order-' + orderId + '.pdf'
+    const docPath = path.join('data', 'order_docs', orderName)
+
+    const doc = new PdfDocument()
+
+    res.setHeader('Content-Type', 'application/pdf')
+    res.setHeader('Content-Disposition', 'filename="'+orderName+'"')
+    doc.pipe(fs.createWriteStream(docPath))
+    doc.pipe(res)
+
+    createPdfContent(doc, order)
+
+    doc.end()
+}
+
+const createPdfContent = (doc, order) =>{
+    doc.fontSize(20).fillColor('#2e8b57').text(order._id)
+    doc.fillColor("#999").text('-------------------------------------------')
+    order.products.forEach((item, index) => {
+        doc.fontSize(10).fillColor("#000").text((index+1)+") "+item.product.title+" : "+item.count+"*"+item.product.price)
+    })
+    doc.fillColor("#999").fontSize(20).text('---')
+    doc.fillColor('#2e8b57').fontSize(15).text("$"+order.total)
 }
